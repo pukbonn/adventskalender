446ad6ca2b0dde330d1eeeae328e3c079efc5ad3
Thomas Rosen <thomas.rosen@me.com>
2020-11-17 20:51:16 +0100

    redirect to https

--- src/index.js
+++ src/index.js
@@ -15,3 +15,10 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+
+if (
+	!window.location.startsWith('https')
+	&& window.location.startsWith('http://adventskalender.puk-bonn.de')
+){
+	window.location.replace(window.location.replace('http://', 'https://'))
+}
