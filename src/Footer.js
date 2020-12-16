d3abdecb4a65fd17fb49b27884520b4dcda97882
thomasrosen <thomas.rosen@me.com>
2020-12-17 00:52:23 +0100

    added info about the code being on GitHub

--- src/Footer.js
+++ src/Footer.js
@@ -32,6 +32,8 @@ function Footer() {
 		</div>
 		<div className="thomasLine">
 			Design und Programmierung von <a href="https://thomasrosen.qiekub.org/" target="_blank" rel="noreferrer">Thomas Rosen</a>.
+			<br/>
+			Der Quellcode des Adventskalenders ist auf <a href="https://github.com/pukbonn/adventskalender" target="_blank" rel="noreferrer">GitHub</a>.
 		</div>
 	</footer>)
 }
