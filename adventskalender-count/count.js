44f40a7b69d6bb9a947201ba15bcc3da32682102
thomasrosen <thomas.rosen@me.com>
2021-01-07 23:19:38 +0100

    added stats about the adventscalender website visits

---
+++ adventskalender-count/count.js
@@ -0,0 +1,50 @@
+const fs = require('fs')
+const csv = require('csv')
+
+/*
+fs.readFile('./Website_Stats - Sheet3.csv', 'utf8', (error, data) => {
+    if (error) {
+        console.log(`Error reading file from disk: ${err}`);
+    } else {
+        const counts =
+            data
+            .split('\n')
+            .map(line => line.substring(1, line.length - 2))
+            .reduce((counts, line) => {
+                counts.add(line)
+                return counts
+            }, new Set())
+
+        console.log(
+            counts.size
+        )
+    }
+})
+*/
+
+fs.readFile('./Website_Stats - Stats.csv', 'utf8', (error, data) => {
+    if (error) {
+        console.log(`Error reading file from disk: ${err}`);
+    } else {
+        csv.parse(data, function (err, data) {
+            data = data
+                .map(line => line[4] + '_' + line[7])
+
+
+            // 131 = line[7]
+            // 761 = line[2]
+            // 144 = line[4]+'_'+line[7]
+            // 764 = line[2]+'_'+line[4]+'_'+line[7]
+            //  11 = line[4]
+
+
+            const counts = data
+                .reduce((counts, line) => {
+                    counts.add(line)
+                    return counts
+                }, new Set())
+
+            console.log(counts.size)
+        })
+    }
+})
