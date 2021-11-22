f5f6d65535bc5e8ab9942ab6ae2b2c6c1c47f8a8
thomasrosen <thomas.rosen@me.com>
2021-11-22 01:15:46 +0100

    added time info in the date card

--- src/Card.js
+++ src/Card.js
@@ -4,6 +4,7 @@ import LazyLoad from 'react-lazyload'
 function Card({data}) {
 	let {
 		date: dateString,
+		time,
 		address,
 		video,
 		photos,
@@ -42,6 +43,15 @@ function Card({data}) {
 			<h2 className="number">{daynumber}</h2>
 			<p className="weekday">{weekday}</p>
 
+			{
+				!!time
+				&& time !== ''
+				? <p className="infoLine time">
+					<svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="black"><path d="M0 0h24v24H0z" fill="none"/><path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/><path d="M12.5 7H11v6l5.25 3.15.75-1.23-4.5-2.67z"/></svg>
+					<span>{time}</span>
+				</p>
+				: null
+			}
 			{
 				!!address
 				&& address !== ''
