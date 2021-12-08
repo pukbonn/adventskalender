603f57e16f9619877579b78b8d0c1eb4805f2930
thomasrosen <thomas.rosen@me.com>
2021-12-08 16:14:40 +0100

    added count to photos

--- src/Card.js
+++ src/Card.js
@@ -78,7 +78,7 @@ function Card({data}) {
 				&& photos.length > 0
 				? <p className="infoLine">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="18px" height="18px"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 5H5v14h14V5zM6 17l3-3.86 2.14 2.58 3-3.87L18 17H6z" opacity=".1"/><path d="M5 21h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2zM5 5h14v14H5V5zm6.14 10.72L9 13.14 6 17h12l-3.86-5.14z"/></svg>
-					<span>Bilder</span>
+					<span>{photos.length === 1 ? 'ein Bild' : `${photos.length} Bilder`}</span>
 				</p>
 				: null
 			}
